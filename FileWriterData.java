0c646012bdefc7da300d521667f7365342f874fa
Gabriel García <100346140@alumnos.uc3m.es>
2018-02-23 10:11:03 +0100

    distancePassedCells en ultima posición rotando M

--- FileWriterData.java
+++ FileWriterData.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileReader; 
 
 import java.nio.file.Files;
 import java.util.*;
