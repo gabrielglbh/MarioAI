5532ae8523d23bee449d4ece67de7cadb08d146d
Gabriel Garcia <gabriel@wifi-84-92.uc3m.es>
2018-03-09 20:31:36 +0100

    Me dejas comerte toda la pollita?

---
+++ script_mario.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+bash compilar.sh
+
+echo "------------ Bot seed = 0 -----------"
+bash ejecutar.sh T3BotAgent -ls 0
+echo "... Execution 1 ..."
+bash ejecutar.sh T3BotAgent -ls 0
+echo "... Execution 2 ..."
+bash ejecutar.sh T3BotAgent -ls 0
+echo "... Execution 3 ..."
+bash ejecutar.sh T3BotAgent -ls 0
+echo "... Execution 4 ..."
+bash ejecutar.sh T3BotAgent -ls 0
+echo "... Execution 5 ..."
+
+echo "------------ Bot seed = 25 -----------"
+bash ejecutar.sh T3BotAgent -ls 25
+echo "... Execution 1 ..."
+bash ejecutar.sh T3BotAgent -ls 25
+echo "... Execution 2 ..."
+bash ejecutar.sh T3BotAgent -ls 25
+echo "... Execution 3 ..."
+bash ejecutar.sh T3BotAgent -ls 25
+echo "... Execution 4 ..."
+bash ejecutar.sh T3BotAgent -ls 25
+echo "... Execution 5 ..."
+
+echo "------------ Bot seed = 50 -----------"
+bash ejecutar.sh T3BotAgent -ls 50
+echo "... Execution 1 ..."
+bash ejecutar.sh T3BotAgent -ls 50
+echo "... Execution 2 ..."
+bash ejecutar.sh T3BotAgent -ls 50
+echo "... Execution 3 ..."
+bash ejecutar.sh T3BotAgent -ls 50
+echo "... Execution 4 ..."
+bash ejecutar.sh T3BotAgent -ls 50
+echo "... Execution 5 ..."
+
+echo "------------ Bot seed = 75 -----------"
+bash ejecutar.sh T3BotAgent -ls 75
+echo "... Execution 1 ..."
+bash ejecutar.sh T3BotAgent -ls 75
+echo "... Execution 2 ..."
+bash ejecutar.sh T3BotAgent -ls 75
+echo "... Execution 3 ..."
+bash ejecutar.sh T3BotAgent -ls 75
+echo "... Execution 4 ..."
+bash ejecutar.sh T3BotAgent -ls 75
+echo "... Execution 5 ..."
+
+echo "------------ Bot seed = 100 -----------"
+bash ejecutar.sh T3BotAgent -ls 100
+echo "... Execution 1 ..."
+bash ejecutar.sh T3BotAgent -ls 100
+echo "... Execution 2 ..."
+bash ejecutar.sh T3BotAgent -ls 100
+echo "... Execution 3 ..."
+bash ejecutar.sh T3BotAgent -ls 100
+echo "... Execution 4 ..."
+bash ejecutar.sh T3BotAgent -ls 100
+echo "... Execution 5 ..."
