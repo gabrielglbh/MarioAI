9941031f01cdb6df98e74a1160f02c1bd1d6ab4b
Gabriel García <100346140@alumnos.uc3m.es>
2018-03-11 18:12:14 +0100

    UPDATED

--- script_mario.sh
+++ script_mario.sh
@@ -3,61 +3,364 @@
 bash compilar.sh
 
 echo "------------ Bot seed = 0 -----------"
-bash ejecutar.sh T3BotAgent -ls 0 -vis off
-echo "... Execution 1 ..."
-bash ejecutar.sh T3BotAgent -ls 0 -vis off
-echo "... Execution 2 ..."
-bash ejecutar.sh T3BotAgent -ls 0 -vis off
-echo "... Execution 3 ..."
-bash ejecutar.sh T3BotAgent -ls 0 -vis off
-echo "... Execution 4 ..."
-bash ejecutar.sh T3BotAgent -ls 0 -vis off
-echo "... Execution 5 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 0
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 0
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 0
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 0
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 0
+echo "... End of Execution 5 ..."
 
 echo "------------ Bot seed = 25 -----------"
-bash ejecutar.sh T3BotAgent -ls 25 -vis off
-echo "... Execution 1 ..."
-bash ejecutar.sh T3BotAgent -ls 25 -vis off
-echo "... Execution 2 ..."
-bash ejecutar.sh T3BotAgent -ls 25 -vis off
-echo "... Execution 3 ..."
-bash ejecutar.sh T3BotAgent -ls 25 -vis off
-echo "... Execution 4 ..."
-bash ejecutar.sh T3BotAgent -ls 25 -vis off
-echo "... Execution 5 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 0
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 0
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 0
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 0
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 0
+echo "... End of Execution 5 ..."
 
 echo "------------ Bot seed = 50 -----------"
-bash ejecutar.sh T3BotAgent -ls 50 -vis off
-echo "... Execution 1 ..."
-bash ejecutar.sh T3BotAgent -ls 50 -vis off
-echo "... Execution 2 ..."
-bash ejecutar.sh T3BotAgent -ls 50 -vis off
-echo "... Execution 3 ..."
-bash ejecutar.sh T3BotAgent -ls 50 -vis off
-echo "... Execution 4 ..."
-bash ejecutar.sh T3BotAgent -ls 50 -vis off 
-echo "... Execution 5 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 0
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 0
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 0
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 0
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 0
+echo "... End of Execution 5 ..."
 
 echo "------------ Bot seed = 75 -----------"
-bash ejecutar.sh T3BotAgent -ls 75 -vis off
-echo "... Execution 1 ..."
-bash ejecutar.sh T3BotAgent -ls 75 -vis off
-echo "... Execution 2 ..."
-bash ejecutar.sh T3BotAgent -ls 75 -vis off
-echo "... Execution 3 ..."
-bash ejecutar.sh T3BotAgent -ls 75 -vis off
-echo "... Execution 4 ..."
-bash ejecutar.sh T3BotAgent -ls 75 -vis off
-echo "... Execution 5 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 0
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 0
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 0
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 0
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 0
+echo "... End of Execution 5 ..."
 
 echo "------------ Bot seed = 100 -----------"
-bash ejecutar.sh T3BotAgent -ls 100 -vis off
-echo "... Execution 1 ..."
-bash ejecutar.sh T3BotAgent -ls 100 -vis off
-echo "... Execution 2 ..."
-bash ejecutar.sh T3BotAgent -ls 100 -vis off
-echo "... Execution 3 ..."
-bash ejecutar.sh T3BotAgent -ls 100 -vis off
-echo "... Execution 4 ..."
-bash ejecutar.sh T3BotAgent -ls 100 -vis off
-echo "... Execution 5 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 0
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 0
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 0
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 0
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 0
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 3000 -----------"
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 0
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 0
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 0
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 0
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 0
+
+echo "------------------------------ DIFICULTAD 1 -----------------------------"
+
+echo "------------ Bot seed = 0 -----------"
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 1
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 1
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 1
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 1
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 1
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 25 -----------"
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 1
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 1
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 1
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 1
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 1
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 50 -----------"
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 1
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 1
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 1
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 1
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 1
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 75 -----------"
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 1
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 1
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 1
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 1
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 1
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 100 -----------"
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 1
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 1
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 1
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 1
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 1
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 3000 -----------"
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 1
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 1
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 1
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 1
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 1
+
+echo "------------------------------ DIFICULTAD 2 -----------------------------"
+
+echo "------------ Bot seed = 0 -----------"
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 2
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 2
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 2
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 2
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 2
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 25 -----------"
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 2
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 2
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 2
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 2
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 2
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 50 -----------"
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 2
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 2
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 2
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 2
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 2
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 75 -----------"
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 2
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 2
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 2
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 2
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 2
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 100 -----------"
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 2
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 2
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 2
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 2
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 2
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 3000 -----------"
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 2
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 2
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 2
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 2
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 2
+
+echo "------------------------------ DIFICULTAD 25 -----------------------------"
+
+echo "------------ Bot seed = 0 -----------"
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 25
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 25
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 25
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 25
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 25
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 25 -----------"
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 25
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 25
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 25
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 25
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 25
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 50 -----------"
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 25
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 25
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 25
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 25
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 25
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 75 -----------"
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 25
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 25
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 25
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 25
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 25
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 100 -----------"
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 25
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 25
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 25
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 25
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 25
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 3000 -----------"
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 25
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 25
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 25
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 25
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 25
+
+echo "------------------------------ DIFICULTAD 45 -----------------------------"
+
+echo "------------ Bot seed = 0 -----------"
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 45
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 45
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 45
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 45
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 0 -ld 45
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 25 -----------"
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 45
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 45
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 45
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 45
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 25 -ld 45
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 50 -----------"
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 45
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 45
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 45
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 45
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 50 -ld 45
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 75 -----------"
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 45
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 45
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 45
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 45
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 75 -ld 45
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 100 -----------"
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 45
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 45
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 45
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 45
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 100 -ld 45
+echo "... End of Execution 5 ..."
+
+echo "------------ Bot seed = 3000 -----------"
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 45
+echo "... End of Execution 1 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 45 
+echo "... End of Execution 2 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 45
+echo "... End of Execution 3 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 45
+echo "... End of Execution 4 ..."
+bash ejecutar.sh T3HumanAgent -ls 3000 -ld 45
